a83137797bce5c95ba0159427da57059cd2055a4
Chandrakant <kokarect07@gmail.com>
2023-08-31 09:57:53 +0530

    image add button add

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
+    <div>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      button
+     </div>
+     <div className="image">
+      <img src="akki.jpg"  alt="akki picture" width="200px" height="200px"/>
+     </div>
     </div>
   );
 }
